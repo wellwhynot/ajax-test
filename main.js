eebccc353d5a65681724bf26f7d83405bf815d4e
wellwhynot <wellwhynot@protonmail.com>
2021-02-19 10:10:55 +0000

    Added truncation of data and table formatting

--- main.js
+++ main.js
@@ -1,19 +1,47 @@
-function getData(cb) {
+const baseURL = "https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
+
     var xhr = new XMLHttpRequest();
 
-    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.open("GET", baseURL + type + "/");
     xhr.send();
 
     xhr.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("data").innerHTML = this.responseText;
             cb(JSON.parse(this.responseText));
         }
     };
 };
 
-function printDataToConsole(data) {
-    console.log(data);
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function (key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
+
 };
 
-getData(printDataToConsole);
+function writeToDocument(type) {
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+    getData(type, function (data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function (item) {
+            var dataRow = [];
+            Object.keys(item).forEach(function(key){
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15);
+                dataRow.push(`<td>${truncatedData}</td>`)
+            });
+            tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+    });
+}
